cd86cdee09eb19e6a16397556c8b5119bb5b1ae3
maissahammami <148292631+maissahammami@users.noreply.github.com>
2025-06-26 02:29:31 +0100

    petite modification

--- src/app/pages/historique/historique.component.ts
+++ src/app/pages/historique/historique.component.ts
@@ -4,6 +4,7 @@ import { UserService } from '../../services/user.service';
 @Component({
   selector: 'app-historique',
   templateUrl: './historique.component.html',
+  styleUrls: ['./historique.component.scss'],
 })
 export class HistoriqueComponent {
   historyList = this.userService.getHistory();
